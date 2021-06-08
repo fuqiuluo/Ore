5c0093fdb56c02c1c560cfbee569c0a26580b2dd
zhangshikj <1372362033@qq.com>
2021-06-08 23:58:53 +0800

    提交工具方法

--- Ore-Base/src/main/java/moe/ore/util/TeaUtil.java
+++ Ore-Base/src/main/java/moe/ore/util/TeaUtil.java
@@ -19,6 +19,27 @@
  *
  */
 
+/*
+ * English :
+ *  The project is protected by the MPL open source agreement.
+ * Open source agreement warning that prohibits deletion of project source code files.
+ * The project is prohibited from acting in illegal areas.
+ * All illegal activities arising from the use of this project are the responsibility of the second author, and the original author of the project is not responsible
+ *
+ *  中文：
+ *  该项目由MPL开源协议保护。
+ *  禁止删除项目源代码文件的开源协议警告内容。
+ * 禁止使用该项目在非法领域行事。
+ * 使用该项目产生的违法行为，由第二作者全责，原作者免责
+ *
+ * 日本语：
+ * プロジェクトはMPLオープンソース契約によって保護されています。
+ *  オープンソース契約プロジェクトソースコードファイルの削除を禁止する警告。
+ * このプロジェクトは違法地域の演技を禁止しています。
+ * このプロジェクトの使用から生じるすべての違法行為は、2番目の著者の責任であり、プロジェクトの元の著者は責任を負いません。
+ *
+ */
+
 /*******************************************************************************
  *  2021 Ore Developer Warn
  *
@@ -49,9 +70,9 @@ public class TeaUtil {
         return crypter.encrypt(data, key);
     }
 
-    public static byte[] encrypt(byte[] data, byte[] key, int len) {
+    public static byte[] encrypt(byte[] data, byte[] key, int offset, int len) {
         Crypter crypter = new Crypter();
-        return crypter.encrypt(data, 0, len, key);
+        return crypter.encrypt(data, offset, len, key);
     }
 
     public static byte[] decrypt(byte[] data, byte[] key) {
@@ -59,8 +80,8 @@ public class TeaUtil {
         return crypter.decrypt(data, key);
     }
 
-    public static byte[] decrypt(byte[] data, byte[] key, int len) {
+    public static byte[] decrypt(byte[] data, byte[] key, int offset, int len) {
         Crypter crypter = new Crypter();
-        return crypter.decrypt(data, 0, len, key);
+        return crypter.decrypt(data, offset, len, key);
     }
 }
--- Ore-Base/src/main/kotlin/moe/ore/helper/bytes/BytePacketExt.kt
+++ Ore-Base/src/main/kotlin/moe/ore/helper/bytes/BytePacketExt.kt
@@ -82,6 +82,27 @@
  *
  */
 
+/*
+ * English :
+ *  The project is protected by the MPL open source agreement.
+ * Open source agreement warning that prohibits deletion of project source code files.
+ * The project is prohibited from acting in illegal areas.
+ * All illegal activities arising from the use of this project are the responsibility of the second author, and the original author of the project is not responsible
+ *
+ *  中文：
+ *  该项目由MPL开源协议保护。
+ *  禁止删除项目源代码文件的开源协议警告内容。
+ * 禁止使用该项目在非法领域行事。
+ * 使用该项目产生的违法行为，由第二作者全责，原作者免责
+ *
+ * 日本语：
+ * プロジェクトはMPLオープンソース契約によって保護されています。
+ *  オープンソース契約プロジェクトソースコードファイルの削除を禁止する警告。
+ * このプロジェクトは違法地域の演技を禁止しています。
+ * このプロジェクトの使用から生じるすべての違法行為は、2番目の著者の責任であり、プロジェクトの元の著者は責任を負いません。
+ *
+ */
+
 /*******************************************************************************
  *  2021 Ore Developer Warn
  *
@@ -108,6 +129,7 @@ package moe.ore.helper.bytes
 
 import kotlinx.io.core.BytePacketBuilder
 import moe.ore.util.BytesUtil
+import moe.ore.util.TeaUtil
 
 fun createBuilder() = BytePacketBuilder()
 
@@ -148,4 +170,10 @@ fun BytePacketBuilder.writeBytes(bytes : ByteArray) = this.writeFully(bytes, 0, 
 
 fun BytePacketBuilder.writeULongToBuf32(v: ULong) {
     this.writeBytes(BytesUtil.int64ToBuf32(v.toLong()))
+}
+
+fun BytePacketBuilder.writeTeaEncrypt(key : ByteArray, block : BytePacketBuilder.() -> Unit) {
+    val body = createBuilder()
+    body.block()
+    this.writeBytes(TeaUtil.encrypt(body.toByteArray(), key))
 }
