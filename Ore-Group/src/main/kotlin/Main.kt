6bbf3e5f05d404673d033b8bf2ffe8808460c219
AutoMagic <3316860990@qq.com>
2021-07-14 10:32:11 +0800

    ipad

--- Ore-Group/src/main/kotlin/Main.kt
+++ Ore-Group/src/main/kotlin/Main.kt
@@ -6,16 +6,30 @@ import moe.ore.api.listener.SmsHelper
 import moe.ore.core.OreBot
 import moe.ore.core.OreManager
 import moe.ore.core.helper.DataManager
+import moe.ore.core.helper.sendPacket
+import moe.ore.core.net.packet.Handler
+import moe.ore.core.net.packet.PacketType
+import moe.ore.core.net.packet.SingleHandler
+import moe.ore.core.protocol.ProtocolInternal
 import moe.ore.core.protocol.tars.service.GrayUin
 import moe.ore.group.TroopManager
+import moe.ore.tars.UniPacket
 import java.io.File
 import java.util.*
 
 fun main() {
     // val ore = OreManager.addBot(203411690, "911586ABc", "C:\Users\13723\Desktop\Ore")
 
-    val ore = OreManager.addBot(3042628723, "911586abcd", File("C:\\Users\\13723\\Desktop\\Ore").absolutePath)
+    val ore = OreManager.addBot(3042628723, "911586abcd", File("").absolutePath)
 
+    DataManager.manager(3042628723).protocolType = ProtocolInternal.ProtocolType.IOS_IPAD
+//    ore.sendPacket("cmd", ByteArray(0), PacketType.ServicePacket, firstToken = ByteArray(0), secondToken = ByteArray(0))
+//    val oreBot1 = ore as OreBot
+//    val singleHandler = SingleHandler(1, "")
+//    oreBot1.client.registerCommonHandler(singleHandler)
+//    if (singleHandler.wait(1000 * 3)) {
+//        val decode = UniPacket.decode(singleHandler.fromService?.body)
+//    }
     ore.oreListener = object : OreListener {
         override fun onStatusChanged(status: OreStatus) {
             println("机器人状态改变为：$status")
